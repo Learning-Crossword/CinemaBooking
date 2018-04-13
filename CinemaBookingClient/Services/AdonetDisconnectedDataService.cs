187e5fcca41b273f2b5528bd9fbb959266b02fab
Learning Crossword <Learning.Crossword@gmail.com>
2018-04-13 16:20:17 +0300

    Immediate commit

--- CinemaBookingClient/Services/AdonetDisconnectedDataService.cs
+++ CinemaBookingClient/Services/AdonetDisconnectedDataService.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using CinemaBookingClient.Models;
+using CinemaBookingClient.Models.DataModel;
 using Microsoft.Extensions.Configuration;
 
 namespace CinemaBookingClient.Services
@@ -22,7 +23,38 @@ namespace CinemaBookingClient.Services
 
         public int CancelTickets(string userId, int cinemaHallId, int seanceId, IEnumerable<Position> removeSeats)
         {
-            throw new NotImplementedException();
+            int cnt = 0;
+            if (removeSeats == null)
+                return 0;
+            var seats = removeSeats.Where(z => z != null);
+            if (seats.Count() == 0)
+                return 0;
+            HashSet<Position> toRemove = new HashSet<Position>(seats);
+
+            using (var conn = new SqlConnection(connectionStr))
+            {
+                string sql = "select * from Tickets";
+                var da = new SqlDataAdapter(sql, conn);
+                var ds = new DataSet();
+                da.Fill(ds, "Tickets");
+                DataTable dt = ds.Tables["Tickets"];
+                foreach (var pos in toRemove)
+                {
+                    object[] findVals = new object[3];
+                    findVals[0] = pos.AreaNumber;
+                    findVals[1] = pos.ColumnIndex;
+                    findVals[2] = pos.RowIndex;
+
+                    DataRow row = dt.Rows.Find(findVals);
+                    int index = dt.Rows.IndexOf(row);
+                    string delSql = "Delete from Tickets where Id = @Id";
+                    da.DeleteCommand = new SqlCommand(delSql, conn);
+                    da.DeleteCommand.Parameters.Add(new SqlParameter("@Id", index));
+                    da.Update(ds.Tables["Tickets"]);
+                    cnt++;
+                }
+            }
+            return cnt;
         }
 
         public Customer CreateCustomer(string aspNetUserid)
@@ -65,7 +97,7 @@ namespace CinemaBookingClient.Services
                         , c.Name as 'CinemaName'
 						, ch.Schema_Url 
                         from CinemaHalls as ch
-                        join (select * from Cinemas) as c 
+                        join Cinemas as c 
                         on ch.CinemaId = c.Id
                         where ch.Id = @Id and ch.CinemaId = @CinemaId";
 
@@ -76,8 +108,9 @@ namespace CinemaBookingClient.Services
                 da.SelectCommand.Parameters.Add(new SqlParameter("@Id", cinemahall_id));
                 da.SelectCommand.Parameters.Add(new SqlParameter("@CinemaId", cinema_id));
                 var ds = new DataSet();
-                da.Fill(ds, "FoundCinemaHall");                
+                da.Fill(ds, "FoundCinemaHall");
                 DataRow foundCinemaHall = ds.Tables[0].Rows[0];
+
                 cinemaHall = new CinemaHall
                 {
                     Id = cinemahall_id,
@@ -122,8 +155,57 @@ namespace CinemaBookingClient.Services
             Customer customer = GetCustomer(aspnetuser_id);
             if (customer == null)
                 return null;
+                        
+            DataTable dt = GetOrdersDB(customer.Id);
+            if(dt.Rows.Count < 0)
+            {
+                return null;
+            }
 
-            IEnumerable<Order> orders = null;
+            List<Order> orders  = new List<Order>();
+            var group = dt.Rows.Cast<DataRow>()
+                .GroupBy(row => row["Id"])
+                .ToList()
+                ;
+
+            foreach (var el in group)
+            {
+                var row = el.FirstOrDefault();
+
+                Order order = new Order
+                {
+                    Id = (int)row["Id"],
+                    CustomerId = (int)row["CustomerId"],
+                    Customer = new Customer
+                    {
+                        Id = (int)row["CustomerId"],
+                        AspNetUsersId = customer.AspNetUsersId
+                        //TODO orders?
+                    },
+                    SeanceId = (int)row["SeanceId"],
+                    Seance = new Seance { },
+                    OrderDate = DateTime.Parse((string)row["OrderDate"])
+                };
+
+                var tickets = el.Select(z => new Ticket
+                {
+                    Id = (int)z["TicketId"],
+                    AreaNumber = (int) z["AreaNumber"],
+                    ColumnIndex = (int)z["ColumnIndex"],
+                    RowIndex = (int)z["RowIndex"],
+                    OrderId = (int)z["Id"]
+                });
+
+                order.Tickets = tickets.ToList();
+                orders.Add(order);
+            }
+
+            return orders;
+        }
+
+        private DataTable GetOrdersDB(int customerId)
+        {
+            DataTable dt = null;
             using (var conn = new SqlConnection(connectionStr))
             {
                 string sql = @"select 
@@ -133,23 +215,27 @@ namespace CinemaBookingClient.Services
 	              , s.CinemaHallId
 	              , s.[Date]
 	              , s.[Time]
-                  FROM[CinemaBooking].[dbo].[Orders] as o
-                  join(select * from[CinemaBooking].[dbo].Seances) as s
-                  on o.SeanceId = s.Id
-                  join(select * from [CinemaBooking].[dbo].CinemaHalls) as ch
-                  on s.CinemaHallId = @AspNetUsersId; 
-                  select * from Tickets as t ";
+                  , t.OrderId
+                  , t.Id as 'TicketId'
+                  FROM Orders as o
+                  join Seances as s
+                    on o.SeanceId = s.Id
+                  join CinemaHalls as ch
+                    on s.CinemaHallId = ch.Id 
+                  join Tickets as t
+			        on o.Id = t.OrderId
+				  where o.CustomerId = @CustomerId";
 
-                //var da = new SqlDataAdapter(sql, conn);
-
-                //da.SelectCommand = new SqlCommand(sql, conn);
-                //da.SelectCommand.Parameters.Add(new SqlParameter("@AspNetUsersId", SqlDbType.NVarChar, "AspNetUsersId"));
-                //DataSet ds = new DataSet();
-                //da.Fill(ds, "Orders");
-                //da.Fill(ds, "Tickets");
-                //ds.Tables[0].Constraints.Add("PK_Customers", ds.Tables[0].Columns[0], true);
+                var da = new SqlDataAdapter(sql, conn)
+                {
+                    SelectCommand = new SqlCommand(sql, conn)
+                };
+                da.SelectCommand.Parameters.Add(new SqlParameter("@CustomerId", customerId));
+                DataSet ds = new DataSet();
+                da.Fill(ds, "Orders");
+                dt = ds.Tables["Orders"];
             }
-            return orders;
+            return dt;
         }
 
         public IEnumerable<Ticket> GetSeanceTickets(int value)
